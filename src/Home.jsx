474d426b4a4e77db8b90b761b2f69fcb89213e52
Pratham Singh Kushwah <prathamsinghkushwah@Prathams-MacBook-Air.local>
2026-07-27 21:41:55 +0530

    Update Trusted By heading text to uppercase: TRUSTED BY 100+ CLIENTS INCLUDIN' MARKETING AGENCIES, STARTUPS AND CREATORS

--- src/Home.jsx
+++ src/Home.jsx
@@ -510,7 +510,7 @@ export default function Home() {
       <div className="container1440 trustedby">
         <div className="mainusp-holder trustedbytxt">
           <h1 words-slide-from-right="" text-split="" className="mainusp trusted-by-text red">
-            hiii
+            TRUSTED BY 100+ CLIENTS INCLUDIN&#39; MARKETING AGENCIES, STARTUPS AND CREATORS
           </h1>
         </div>
         <div className="subheading-cta-holder">
