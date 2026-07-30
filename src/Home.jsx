32823f8da37ac87f2eb58757a8a806da98a68a07
Pratham Singh Kushwah <prathamsinghkushwah@Prathams-MacBook-Air.local>
2026-07-30 22:05:06 +0530

    Add two more client testimonials (Rohan Kapoor & Priya Nair)

--- src/Home.jsx
+++ src/Home.jsx
@@ -1446,6 +1446,78 @@ export default function Home() {
                   </div>
                 </div>
               </div>
+
+              {/* Testimonial 7 (Added) */}
+              <div className="uui-testimonial16_content-2 black">
+                <div className="uui-testimonial16_rating-wrapper-2">
+                  {[...Array(5)].map((_, i) => (
+                    <div key={i} className="uui-testimonial16_rating-icon-2 w-embed">
+                      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clipPath="url(#clip0_28_8746)">
+                          <path d="M9.53834 1.60996C9.70914 1.19932 10.2909 1.19932 10.4617 1.60996L12.5278 6.57744C12.5998 6.75056 12.7626 6.86885 12.9495 6.88383L18.3123 7.31376C18.7556 7.3493 18.9354 7.90256 18.5976 8.19189L14.5117 11.6919C14.3693 11.8139 14.3071 12.0053 14.3506 12.1876L15.5989 17.4208C15.7021 17.8534 15.2315 18.1954 14.8519 17.9635L10.2606 15.1592C10.1006 15.0615 9.89938 15.0615 9.73937 15.1592L5.14806 17.9635C4.76851 18.1954 4.29788 17.8534 4.40108 17.4208L5.64939 12.1876C5.69289 12.0053 5.6307 11.8139 5.48831 11.6919L1.40241 8.19189C1.06464 7.90256 1.24441 7.3493 1.68773 7.31376L7.05054 6.88383C7.23744 6.86885 7.40024 6.75056 7.47225 6.57744L9.53834 1.60996Z" fill="currentColor"></path>
+                        </g>
+                      </svg>
+                    </div>
+                  ))}
+                </div>
+                <div className="uui-heading-xxsmall-2 text-weight-medium white">
+                  Media Mansion brings pure magic to every campaign! Their team took our vision, elevated it beyond what we imagined, and delivered a brand identity that truly stands out in a crowded market. Responsive, visionary, and extremely fun to work with!
+                </div>
+                <div className="uui-testimonial16_client-2">
+                  <div className="uui-testimonial16_client-image-wrapper-2">
+                    <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=200&auto=format&fit=crop" loading="lazy" alt="Rohan Kapoor" className="uui-testimonial16_customer-image-2" style={{ width: '48px', height: '48px', borderRadius: '50%', objectFit: 'cover' }} />
+                  </div>
+                  <div className="uui-testimonial16_client-info-2">
+                    <div className="uui-testimonial16_client-heading-2 white">
+                      Rohan Kapoor
+                    </div>
+                    <div className="uui-text-size-small-2 white">
+                      Founder &amp; Creative Director
+                      <br />
+                      at Starlight Studios
+                    </div>
+                  </div>
+                  <div className="uui-testimonial16_client-image-wrapper-2">
+                    <img src={starPurple} loading="lazy" alt="" className="uui-testimonial16_customer-image-2 star" />
+                  </div>
+                </div>
+              </div>
+
+              {/* Testimonial 8 (Added) */}
+              <div className="uui-testimonial16_content-2">
+                <div className="uui-testimonial16_rating-wrapper-2">
+                  {[...Array(5)].map((_, i) => (
+                    <div key={i} className="uui-testimonial16_rating-icon-2 w-embed">
+                      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clipPath="url(#clip0_28_8746)">
+                          <path d="M9.53834 1.60996C9.70914 1.19932 10.2909 1.19932 10.4617 1.60996L12.5278 6.57744C12.5998 6.75056 12.7626 6.86885 12.9495 6.88383L18.3123 7.31376C18.7556 7.3493 18.9354 7.90256 18.5976 8.19189L14.5117 11.6919C14.3693 11.8139 14.3071 12.0053 14.3506 12.1876L15.5989 17.4208C15.7021 17.8534 15.2315 18.1954 14.8519 17.9635L10.2606 15.1592C10.1006 15.0615 9.89938 15.0615 9.73937 15.1592L5.14806 17.9635C4.76851 18.1954 4.29788 17.8534 4.40108 17.4208L5.64939 12.1876C5.69289 12.0053 5.6307 11.8139 5.48831 11.6919L1.40241 8.19189C1.06464 7.90256 1.24441 7.3493 1.68773 7.31376L7.05054 6.88383C7.23744 6.86885 7.40024 6.75056 7.47225 6.57744L9.53834 1.60996Z" fill="currentColor"></path>
+                        </g>
+                      </svg>
+                    </div>
+                  ))}
+                </div>
+                <div className="uui-heading-xxsmall-2 text-weight-medium">
+                  The speed and quality of work from Media Mansion is unmatched. They handled our multi-channel ad campaigns and video assets seamlessly, boosting our conversion rates by 45% in the first month alone!
+                </div>
+                <div className="uui-testimonial16_client-2">
+                  <div className="uui-testimonial16_client-image-wrapper-2">
+                    <img src="https://images.unsplash.com/photo-1517841905240-472988babdf9?q=80&w=200&auto=format&fit=crop" loading="lazy" alt="Priya Nair" className="uui-testimonial16_customer-image-2" style={{ width: '48px', height: '48px', borderRadius: '50%', objectFit: 'cover' }} />
+                  </div>
+                  <div className="uui-testimonial16_client-info-2">
+                    <div className="uui-testimonial16_client-heading-2">
+                      Priya Nair
+                    </div>
+                    <div className="uui-text-size-small-2">
+                      Head of Growth
+                      <br />
+                      at Artisan &amp; Co.
+                    </div>
+                  </div>
+                  <div className="uui-testimonial16_client-image-wrapper-2">
+                    <img src={starPurple} loading="lazy" alt="" className="uui-testimonial16_customer-image-2 star" />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
